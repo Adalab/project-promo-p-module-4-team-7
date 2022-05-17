2d574944beecd4e307a29a075e26c7795a3f91d9
Marta Torres Burguillo <99444191+burguimarta@users.noreply.github.com>
2022-05-17 13:20:06 +0200

    add endpoints to index.js

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+uuidv4();
 
 const server = express();
 
 server.use(cors());
-server.use(express.json());
-
+server.use(express.json({
+    limit: "10mb"
+}));
+server.set('view engine', 'ejs')
+// Arrancamos el servividor en el puerto 4000
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.post("/card", (req, res)=> {
-    const responseSuccess={
-        success:true,
-        cardURL: "https://awesome-profile-cards.herokuapp.com/card"
-    }
-    const responseError={
-        success:false,
-        error:"faltan datos"
-    }
-    res.json();
+const savedCards = [];
+// Escribimos los endpoints que queramos
+server.post("/card", (req, res) => {
+  const requestData =
+    req.body.name !== "" &&
+    req.body.job !== "" &&
+    req.body.photo !== "" &&
+    req.body.email !== "" &&
+    req.body.linkedin !== "" &&
+    req.body.github !== "";
+  if (requestData) {
+    const newCard = { ...req.body, id: uuidv4() };
+    savedCards.push(newCard);
+
+    const responseSucess = {
+      success: true,
+      cardURL: `https://localhost:4000/card/${newCard.id}`,
+    };
+    res.json(responseSucess);
+  } else {
+    const responseError = {
+      success: false,
+      error: "Faltan datos",
+    };
+    res.json(responseError);
+  }
 });
