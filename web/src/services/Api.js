8292b48795d4706a60adaf8db46c0bf1ce2aa922
Marta Torres Burguillo <99444191+burguimarta@users.noreply.github.com>
2022-05-23 21:22:06 +0200

    try to fix errors for heroku

--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -1,5 +1,5 @@
 const dataApi = (dataCard) => {
-  return fetch('https://awesome-profile-cards-tbeach.herokuapp/card', {
+  return fetch('https://awesome-profile-cards-tbeach.herokuapp.com/card', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(dataCard),
